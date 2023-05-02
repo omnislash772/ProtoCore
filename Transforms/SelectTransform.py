2d13a062802479aa7092015f209165a8f92c4e5b
Drekk <drekk@vk3mag.net>
2023-05-02 23:54:29 +1000

    Added SelectTransform Added Omnis faces

---
+++ Transforms/SelectTransform.py
@@ -0,0 +1,27 @@
+from Transforms import Transform
+from PIL import Image
+
+class SelectTransform(Transform.Transform):
+    def __init__(self, name, **kwargs):
+        super().__init__(name, **kwargs)
+        self.defaultFrame = Image.new(mode="RGBA", size=(1,1))
+
+    def process(self, inputFrames, vars):
+        if type(self.select) == str:
+            if self.select in vars.keys() and type(vars[self.select]) == int:
+                inputId = vars[self.select] % len(self.inputs)
+                return inputFrames[self.inputs[inputId]]
+        elif type(self.select) == int:
+            return inputFrames[self.inputs[self.select % len(self.inputs)]]
+
+        return self.defaultFrame
+
+    def getArgs(self):
+        return {
+            "inputs": {
+                "types": [list]
+            },
+            "select": {
+                "types": [int, str]
+            }
+        }
